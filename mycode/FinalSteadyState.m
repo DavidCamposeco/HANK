e6b57d649eaf09cd378eecf2f674e785461346e8
DavidCamposeco <David.Camposeco.Paulsen@gmail.com>
2019-03-10 11:52:20 -0600

    folder cleanup

--- mycode/FinalSteadyState.m
+++
@@ -1,5 +0,0 @@
-if Display; fprintf('Solving for final steady state\n'); end
-
-solnFINALSS = solnINITSS;
-equmFINALSS = equmINITSS;
-statsFINALSS = statsINITSS;
--- mycode/Main.m
+++
@@ -1,9 +0,0 @@
-clear
-SetParameters
-% distcomp.feature('LocalUseMpiexec',false);parpool('local',ngpy);
-Procedures
-if CalibrateCostFunction; Calibration; end
-InitialSteadyState
-SaveSteadyStateOutput
-FinalSteadyState
-if DoImpulseResponses; ImpulseResponses; end
